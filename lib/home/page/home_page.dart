f6f5ffc6d9bf2977ad770e388258dfa5c1f35085
erjigit12 <bvjcjcgjf@gmail.com>
2023-03-05 18:24:55 +0600

    updated

---
+++ lib/home/page/home_page.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyHomePage'),
+      ),
+    );
+  }
+}
